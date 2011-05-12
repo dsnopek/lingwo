71a936932798ee9a9c1fbbf2c9d365fcbdcbdfae
David Snopek <dsnopek@gmail.com>
2011-05-12 14:37:59 -0500

    Got the pron data actually saving and displaying.

---
+++ lingwo_pron/lingwo-pron.tpl.php
@@ -0,0 +1,15 @@
+<div class="lingwo-pron-list">
+<?php foreach ($entry->pron as $pron): ?>
+  <div class="lingwo-pron-item">
+  <?php if (!empty($pron['tag'])): ?>
+    <span class="lingwo-pron-tag">(<?php print $pron['tag']; ?>)</span>
+  <?php endif; ?>
+  <?php if (!empty($pron['ipa'])): ?>
+    <span class="lingwo-pron-ipa">/<?php print $pron['ipa']; ?>/</span>
+  <?php endif; ?>
+  <?php if (!empty($pron['audio'])): ?>
+    <a href="<?php print $pron['audioUrl']; ?>" class="lingwo-pron-audio">Listen</a>
+  <?php endif; ?>
+  </div>
+<?php endforeach; ?>
+</div>
