be85d4a2240965ad338ad94d40ed556a5c7022af
David Snopek <dsnopek@gmail.com>
2011-05-12 15:44:59 -0500

    * Style the pron.
     * Show translated human tag.

--- lingwo_pron/lingwo-pron.tpl.php
+++ lingwo_pron/lingwo-pron.tpl.php
@@ -1,15 +1,18 @@
 <?php
+global $language;
 
 if ($source_node = $entry->getTranslationSource()) {
   // display the pron as if this were the source node
   $entry = LingwoEntry::fromNode($source_node);
 }
+
+drupal_add_css(drupal_get_path('module', 'lingwo_pron') . '/lingwo_pron.css');
 ?>
 <div class="lingwo-pron-list">
 <?php foreach ($entry->pron as $pron): ?>
   <div class="lingwo-pron-item">
   <?php if (!empty($pron['tag'])): ?>
-    <span class="lingwo-pron-tag">(<?php print $pron['tag']; ?>)</span>
+    <span class="lingwo-pron-tag">(<?php print LingwoPron::getTagOptions($language->language, TRUE, $pron['tag']); ?>)</span>
   <?php endif; ?>
   <?php if (!empty($pron['ipa'])): ?>
     <span class="lingwo-pron-ipa">/<?php print $pron['ipa']; ?>/</span>
