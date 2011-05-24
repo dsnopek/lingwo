546211b140eaa275c9f5316c877948cf15f7e683
David Snopek <dsnopek@gmail.com>
2011-05-24 12:38:11 -0500

    lingwo_pron: Check file extension and size on upload.

--- lingwo_pron/lingwo_pron.js
+++ lingwo_pron/lingwo_pron.js
@@ -1,12 +1,23 @@
 
 Drupal.behaviors.lingwo_pron = function (context) {
-  // bind to language and remove checkboxes to trigger AHAH
-  $('#edit-language, .lingwo-pron-remove', context).change(function () {
+  function triggerAhah(evt) {
+    evt.preventDefault();
     var id = 'edit--lingwo-pron-refresh',
       settings = Drupal.settings.ahah[id],
       event_name = settings['event'],
       selector = settings['selector'];
     $(selector).trigger(event_name);
-  });
+    return false;
+  }
+
+  // bind to language and remove checkboxes to trigger AHAH
+  $('#edit-language, .lingwo-pron-remove', context).change(triggerAhah);
+  // bind to the upload button as well
+  $('.lingwo-pron-upload', context).click(triggerAhah);
 };
+
+// Replace Drupal.ahahError to give a sane message to users
+Drupal.ahahError = function (xmlhttp, uri) {
+  return xmlhttp.responseText;
+}
 
